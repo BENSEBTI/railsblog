75562fb28fd94346601f16064d747fa82f650b18
bensebti <bensebti.djamel@gmail.com>
2016-03-04 15:48:04 +0000

    fixed comment issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,29 @@
 class CommentsController < ApplicationController
+	
+	def index
+	@article = Article.find(params[:article_id])
+	@comment = @article.comments.all
+	end
 
 	def create
 		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create(article_params)
+		@comment = @article.comments.new(article_params)
+		@comment.user_id = current_user.id
+		@comment.save
 		redirect_to article_path(@article)
+	end
+	
+	def destroy
+	@article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
 	end
 
 	private
 
 	def article_params
-		params.require(:comment).permit(:user, :email, :commentaire)
+		params.require(:comment).permit(:user, :email, :commentaire, :user_id)
 	end
 
 	
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301153350) do
+ActiveRecord::Schema.define(version: 20160302160515) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160301153350) do
     t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
